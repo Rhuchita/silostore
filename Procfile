6959c3b8a1c6d174a8b01fc5d4d83609ab9abb25
Shirude Rhuchita <56022804+Rhuchita@users.noreply.github.com>
2021-08-29 21:38:53 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn silostore.wsgi
---
+++ silostore_web/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User, Bid, Item, ItemComment, ItemCategory
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Item)
+admin.site.register(Bid)
+admin.site.register(ItemComment)
+admin.site.register(ItemCategory)
